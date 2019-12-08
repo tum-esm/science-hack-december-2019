e22220920d22ad22bb22dbbda78e174e5137994c
Moritz Makowski <makowskimoritz@gmail.com>
2019-12-08 15:19:25 +0100

    Added maximum to dataset api

--- flask_backend/processing.py
+++ flask_backend/processing.py
@@ -5,69 +5,88 @@ import csv
 import netCDF4 as nc
 import numpy as np
 import json
-
 import ssl
+import time
+
 
 if not os.environ.get('PYTHONHTTPSVERIFY', '') and getattr(ssl, '_create_unverified_context', None):
     ssl._create_default_https_context = ssl._create_unverified_context
 
 
-def _transform_ODIAC(dataset, months):
+def _scale(mx, factor):
+    """scales matrix mx by factor"""
+    a, b = mx.shape
+    if a % factor != 0 or b % factor != 0:
+        print('Invalid scaling factor!')
+        exit(1)
+    mx_sc = np.zeros([a//factor, b//factor], dtype=np.float64)
+    for new_lat_index in range(a//factor):
+        for new_lon_index in range(b//factor):
+            mx_sc[new_lat_index][new_lon_index] = np.average([row[new_lon_index*factor:(new_lon_index+1)*factor] for row in mx[new_lat_index*factor:(new_lat_index+1)*factor]])
+    return mx_sc
+
+def _transform_ODIAC(dataset, months, scale=1):
     """Transform ODIAC netCDF files to json"""
     records_full = []
+    maximum_l = []
     for month in months:
         print('Construct empty emission matrix')
         emission_matrix_1_0 = np.zeros([180, 360], dtype=np.float64)
-        emission_matrix_1_0[:][:] = dataset["land"][month - 1][:][:]
+        emission_matrix_1_0[:][:] = dataset["land"][month-1][:][:]
+
+        if scale > 1:
+            print("Scale down")
+            matrix_scaled = _scale(emission_matrix_1_0, scale)
+        else:
+            matrix_scaled = emission_matrix_1_0
 
         print('Normalize')
-        emission_matrix_1_0 *= 1 / emission_matrix_1_0.max()  # normalize
+        maximum = matrix_scaled.max()
+        maximum_l.append(maximum)
+        matrix_scaled *= 1/maximum  # normalize
 
         print('Construct record')
         records = []
-        for new_lat_index in range(180):
-            for new_lon_index in range(360):
-                tmp = emission_matrix_1_0[new_lat_index][new_lon_index]
+        for new_lat_index in range(180//scale):
+            for new_lon_index in range(360//scale):
+                tmp = matrix_scaled[new_lat_index][new_lon_index]
                 if tmp > 0.01:
                     records.append({
                         "coordinates": [new_lat_index + 0.5 - 90, (new_lon_index + 0.5 - 180)],
                         "value": tmp
                     })
         records_full.append(records)
-    return records_full
+    return records_full, maximum_l
 
 
-def _transform_EDGAR(dataset):
+def _transform_EDGAR(dataset, scale=1):
     """Transform EDGAR netCDF files to json"""
     print('Construct empty emission matrix')
     emission_matrix_0_1 = np.zeros([1800, 3600], dtype=np.float64)
-    emission_matrix_1_0 = np.zeros([180, 360], dtype=np.float64)
     emission_matrix_0_1[:][:] = dataset['emi_co2'][:][:]
 
-    print("Scale down by 10")
-    for new_lat_index in range(180):
-        for new_lon_index in range(360):
-            emission_matrix_1_0[new_lat_index][new_lon_index] = np.average(
-                [row[new_lon_index * 10:(new_lon_index + 1) * 10] for row in
-                 emission_matrix_0_1[new_lat_index * 10:(new_lat_index + 1) * 10]])
+    scale *= 10
+    print("Scale down")
+    emission_matrix_1_0 = _scale(emission_matrix_0_1, scale)
 
     print('Normalize')
-    emission_matrix_1_0 *= 1 / emission_matrix_1_0.max()  # normalize
+    maximum = emission_matrix_1_0.max()
+    emission_matrix_1_0 *= 1/maximum  # normalize
 
     print('Construct record')
     records = []
-    for new_lat_index in range(180):
-        for new_lon_index in range(360):
+    for new_lat_index in range(1800//scale):
+        for new_lon_index in range(3600//scale):
             tmp = emission_matrix_1_0[new_lat_index][new_lon_index]
             if tmp > 0.01:
                 records.append({
                     "coordinates": [new_lat_index + 0.5 - 90, (new_lon_index + 0.5 - 180)],
                     "value": tmp
                 })
-    return records
+    return records, maximum
 
 
-def get_json_from_url(org, url, months=None):
+def get_json_from_url(org, url, months=None, scale=1):
     """Give url, get fancy json from them netCDF fiiiiiiiles"""
     print('Download netCDF file')
     bufferpath = 'tmp.nc'  # path were the nc file is saved temporarily and deleted after use
@@ -76,20 +95,20 @@ def get_json_from_url(org, url, months=None):
 
     print('Read netCDF file')
     ds = nc.Dataset(bufferpath, "r", format="NETCDF4")
-    records = _transform_EDGAR(ds) if org == 'edgar' else _transform_ODIAC(ds, months)
+    records, maximum = _transform_EDGAR(ds, scale) if org=='edgar' else _transform_ODIAC(ds, months, scale)
 
-    # print("Dumping to json")
-    # with open('out.json', 'w') as outfile:
+    #print("Dumping to json")
+    #with open('out.json', 'w') as outfile:
     #    json.dump(r, outfile)
 
     print('Close and remove netCDF file')
     ds.close()
     os.remove(bufferpath)
 
-    return records
+    return records, maximum
 
 
-def get_records(org, ym):
+def get_records(org, ym, scale=1):
     """get records from organisation org
 
     ym format:
@@ -103,22 +122,57 @@ def get_records(org, ym):
     records_full = []
     years = list(ym.keys())
     years.sort(reverse=False)
+    maximum_l = []
     if org == 'edgar':
         for year in years:  # edgar files only have years
-            records_full.append(get_json_from_url(org, '%s%s_%s.nc' % (begin, org, str(year))))
+            tt, mm = get_json_from_url(
+                org,
+                '%s%s_%s.nc' % (begin, org, str(year)),
+                scale
+            )
+            records_full.append(tt)
+            maximum_l.append(mm)
     else:
         for year in years:
-            records_full += get_json_from_url(org, '%s%s_%s.nc' % (begin, org, str(year)), ym[year])
-    return {"Status": "Ok",
-            "Datasets": records_full}, 200
+            tt, mm = get_json_from_url(
+                org,
+                '%s%s_%s.nc' % (begin, org, str(year)),
+                ym[year],
+                scale
+            )
+            records_full += tt
+            maximum_l += mm
+    # maximum umrechnen in micro gram/m^2/second
+    if org == 'edgar':
+        # netCDF edgar values are provided in kg CO2/m^2/s
+        for i in range(len(maximum_l)):
+            maximum_l[i] = maximum_l[i] * 1000000000
+    else:
+        # netCDF odiac values are provided in gram carbon/m^2/day (monthly mean) of CO2
+        for i in range(len(maximum_l)):
+            maximum_l[i] = maximum_l[i] * 1000000 / 86400
+    print(maximum_l)
+    return {
+        "Status": "Ok",
+        "Datasets": records_full,
+        "Maximum": maximum_l,
+    }, 200
 
 
-# HOW TO USE
 if __name__ == "__main__":
+    # HOW TO USE
+    start = time.time()
     records = get_records(
-        'edgar',
+        'odiac',
         {
-            2014: [9, 10, 11, 12],
-            2015: [1, 2, 3]
-        }
+            2013: [1, 2, 3],
+            2014: [1, 2, 3],
+        },
+        3
     )
+    print(time.time() - start)
+
+
+#? color coding
+#? scale function for 3 deg
+
