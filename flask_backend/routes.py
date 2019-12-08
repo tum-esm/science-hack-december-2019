bbbfdc5dc528a7a9a8eaed30598a2ea65cfffcc2
Moritz Makowski <makowskimoritz@gmail.com>
2019-12-08 15:38:36 +0100

    generated production build of the react frontend

--- flask_backend/routes.py
+++ flask_backend/routes.py
@@ -1,6 +1,10 @@
 from flask_backend import app
+from flask import render_template
 
 @app.route("/status", methods=["GET"])
-def index():
+def status():
     return {"Status": "Ok"}, 200
 
+@app.route("/", methods=["GET"])
+def frontend():
+    return render_template("index.html")
---
+++ flask_backend/static/react/precache-manifest.be9a6e2beda410acd324119e08fc50c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a912c4d05f699f8e5cda2bfd65fa6da3",
+    "url": "/static/react/../../templates/index.html"
+  },
+  {
+    "revision": "8d2ba305d777571b9b23",
+    "url": "/static/react/css/main.327b9e34.chunk.css"
+  },
+  {
+    "revision": "e792ca0a013fc9f8f90f",
+    "url": "/static/react/js/2.0f7b3c49.chunk.js"
+  },
+  {
+    "revision": "0f9f1f461bac7f17f46d54e23ba9eea0",
+    "url": "/static/react/js/2.0f7b3c49.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8d2ba305d777571b9b23",
+    "url": "/static/react/js/main.152161a0.chunk.js"
+  },
+  {
+    "revision": "d514c0967e695d7d7f15",
+    "url": "/static/react/js/runtime-main.21d12ad3.js"
+  }
+]);
